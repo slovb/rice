bb43cf6598ff237633dbfa387e455781080e4011
Rickard Nyman <nyman.rickard@gmail.com>
2020-09-26 23:54:21 +0200

    Not waterlogging during ticks

--- src/main/java/com/slov/rice/objects/blocks/RiceCropBlock.java
+++ src/main/java/com/slov/rice/objects/blocks/RiceCropBlock.java
@@ -1,12 +1,13 @@
 package com.slov.rice.objects.blocks;
 
+import java.util.Random;
+
 import javax.annotation.Nullable;
 
 import com.slov.rice.init.ModItems;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
 import net.minecraft.block.CropsBlock;
 import net.minecraft.block.IWaterLoggable;
 import net.minecraft.fluid.Fluids;
@@ -23,6 +24,7 @@ import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.IWorld;
+import net.minecraft.world.World;
 
 public class RiceCropBlock extends CropsBlock implements IWaterLoggable {
 	public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
@@ -39,7 +41,41 @@ public class RiceCropBlock extends CropsBlock implements IWaterLoggable {
 
 	public RiceCropBlock(Properties builder) {
 		super(builder);
-		this.setDefaultState(this.stateContainer.getBaseState().with(WATERLOGGED, Boolean.valueOf(true)));
+		this.setDefaultState(this.stateContainer.getBaseState().with(this.getAgeProperty(), Integer.valueOf(0))
+				.with(WATERLOGGED, Boolean.valueOf(false)));
+	}
+
+	public BlockState withAgeWaterlogged(int age, boolean waterlogged) {
+		return super.withAge(age).with(WATERLOGGED, Boolean.valueOf(waterlogged));
+	}
+
+	@Override
+	public void grow(World worldIn, BlockPos pos, BlockState state) {
+		int i = this.getAge(state) + this.getBonemealAgeIncrease(worldIn);
+		int j = this.getMaxAge();
+		if (i > j) {
+			i = j;
+		}
+
+		worldIn.setBlockState(pos, this.withAgeWaterlogged(i, state.get(WATERLOGGED)), 2);
+	}
+
+	@Override
+	public void tick(BlockState state, World worldIn, BlockPos pos, Random random) {
+		super.tick(state, worldIn, pos, random);
+		if (!worldIn.isAreaLoaded(pos, 1))
+			return; // Forge: prevent loading unloaded chunks when checking neighbor's light
+		if (worldIn.getLightSubtracted(pos, 0) >= 9) {
+			int i = this.getAge(state);
+			if (i < this.getMaxAge()) {
+				float f = getGrowthChance(this, worldIn, pos);
+				if (net.minecraftforge.common.ForgeHooks.onCropsGrowPre(worldIn, pos, state,
+						random.nextInt((int) (25.0F / f) + 1) == 0)) {
+					worldIn.setBlockState(pos, this.withAgeWaterlogged(i + 1, state.get(WATERLOGGED)), 2);
+					net.minecraftforge.common.ForgeHooks.onCropsGrowPost(worldIn, pos, state);
+				}
+			}
+		}
 	}
 
 	@Override
@@ -65,41 +101,20 @@ public class RiceCropBlock extends CropsBlock implements IWaterLoggable {
 			BlockPos currentPos, BlockPos facingPos) {
 		if (stateIn.get(WATERLOGGED)) {
 			worldIn.getPendingFluidTicks().scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickRate(worldIn));
-			// doesn't do what I think it should do (return water block if killed)
-			/*
-			if (!stateIn.isValidPosition(worldIn, currentPos)) {
-				return Blocks.WATER.getDefaultState();
-			}
-			*/
 		}
 
 		return super.updatePostPlacement(stateIn, facing, facingState, worldIn, currentPos, facingPos);
-		/*
-		return facing == Direction.DOWN && !this.isValidPosition(stateIn, worldIn, currentPos)
-				? Blocks.AIR.getDefaultState()
-				: super.updatePostPlacement(stateIn, facing, facingState, worldIn, currentPos, facingPos);
-				*/
 	}
-	
+
 	@Override
 	protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
 		super.fillStateContainer(builder);
 		builder.add(WATERLOGGED);
 	}
-	
+
+	@SuppressWarnings("deprecation")
 	@Override
 	public IFluidState getFluidState(BlockState state) {
 		return state.get(WATERLOGGED) ? Fluids.WATER.getStillFluidState(false) : super.getFluidState(state);
 	}
-
-	/*
-	 * @Override public BlockState updatePostPlacement(BlockState stateIn, Direction
-	 * facing, BlockState facingState, IWorld worldIn, BlockPos currentPos, BlockPos
-	 * facingPos) { return stateIn; }
-	 */
-
-	/*
-	 * @Override public boolean isValidPosition(BlockState state, IWorldReader
-	 * worldIn, BlockPos pos) { return super.isValidPosition(state, worldIn, pos); }
-	 */
 }
