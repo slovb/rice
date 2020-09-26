5d2a1546f422de68aae41a4454e07ab4a993b416
Rickard Nyman <nyman.rickard@gmail.com>
2020-09-26 22:52:43 +0200

    Renders water

--- src/main/java/com/slov/rice/objects/blocks/RiceCropBlock.java
+++ src/main/java/com/slov/rice/objects/blocks/RiceCropBlock.java
@@ -1,17 +1,31 @@
 package com.slov.rice.objects.blocks;
 
+import javax.annotation.Nullable;
+
 import com.slov.rice.init.ModItems;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.CropsBlock;
+import net.minecraft.block.IWaterLoggable;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.fluid.IFluidState;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.BooleanProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.tags.FluidTags;
+import net.minecraft.util.Direction;
 import net.minecraft.util.IItemProvider;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorld;
 
-public class RiceCropBlock extends CropsBlock {
+public class RiceCropBlock extends CropsBlock implements IWaterLoggable {
+	public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
 
 	private static final VoxelShape[] SHAPE_BY_AGE = new VoxelShape[] {
 			Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 2.0D, 16.0D),
@@ -25,6 +39,15 @@ public class RiceCropBlock extends CropsBlock {
 
 	public RiceCropBlock(Properties builder) {
 		super(builder);
+		this.setDefaultState(this.stateContainer.getBaseState().with(WATERLOGGED, Boolean.valueOf(true)));
+	}
+
+	@Override
+	@Nullable
+	public BlockState getStateForPlacement(BlockItemUseContext context) {
+		IFluidState ifluidstate = context.getWorld().getFluidState(context.getPos());
+		return this.getDefaultState().with(WATERLOGGED,
+				Boolean.valueOf(ifluidstate.isTagged(FluidTags.WATER) && ifluidstate.getLevel() == 8));
 	}
 
 	@Override
@@ -36,4 +59,47 @@ public class RiceCropBlock extends CropsBlock {
 	public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
 		return SHAPE_BY_AGE[state.get(this.getAgeProperty())];
 	}
+
+	@Override
+	public BlockState updatePostPlacement(BlockState stateIn, Direction facing, BlockState facingState, IWorld worldIn,
+			BlockPos currentPos, BlockPos facingPos) {
+		if (stateIn.get(WATERLOGGED)) {
+			worldIn.getPendingFluidTicks().scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickRate(worldIn));
+			// doesn't do what I think it should do (return water block if killed)
+			/*
+			if (!stateIn.isValidPosition(worldIn, currentPos)) {
+				return Blocks.WATER.getDefaultState();
+			}
+			*/
+		}
+
+		return super.updatePostPlacement(stateIn, facing, facingState, worldIn, currentPos, facingPos);
+		/*
+		return facing == Direction.DOWN && !this.isValidPosition(stateIn, worldIn, currentPos)
+				? Blocks.AIR.getDefaultState()
+				: super.updatePostPlacement(stateIn, facing, facingState, worldIn, currentPos, facingPos);
+				*/
+	}
+	
+	@Override
+	protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+		super.fillStateContainer(builder);
+		builder.add(WATERLOGGED);
+	}
+	
+	@Override
+	public IFluidState getFluidState(BlockState state) {
+		return state.get(WATERLOGGED) ? Fluids.WATER.getStillFluidState(false) : super.getFluidState(state);
+	}
+
+	/*
+	 * @Override public BlockState updatePostPlacement(BlockState stateIn, Direction
+	 * facing, BlockState facingState, IWorld worldIn, BlockPos currentPos, BlockPos
+	 * facingPos) { return stateIn; }
+	 */
+
+	/*
+	 * @Override public boolean isValidPosition(BlockState state, IWorldReader
+	 * worldIn, BlockPos pos) { return super.isValidPosition(state, worldIn, pos); }
+	 */
 }
