412c8aabba5eef24a5edc2054757a2c4456e7f14
Rickard Nyman <nyman.rickard@gmail.com>
2020-10-28 00:27:17 +0100

    removed unused line

--- src/main/java/com/slov/rice/Rice.java
+++ src/main/java/com/slov/rice/Rice.java
@@ -1,6 +1,5 @@
 package com.slov.rice;
 
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -22,7 +21,6 @@ public class Rice
 
     public Rice() {
     	IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-    	MinecraftForge.EVENT_BUS.register(this);
     	
     	ModBlocks.BLOCKS.register(bus);
     	ModItems.ITEMS.register(bus);
